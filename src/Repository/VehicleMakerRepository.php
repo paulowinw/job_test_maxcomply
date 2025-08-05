b0537d9401a8e8379da706e0111f5c4f02b853fa
Paulo Lopes <pauloandreh.ti@gmail.com>
2025-08-05 16:41:56 +0100

    Adding the method findMakersByVehicleType tp the VehicleMakerRepository

--- src/Repository/VehicleMakerRepository.php
+++ src/Repository/VehicleMakerRepository.php
@@ -16,28 +16,13 @@ class VehicleMakerRepository extends ServiceEntityRepository
         parent::__construct($registry, VehicleMaker::class);
     }
 
-//    /**
-//     * @return VehicleMaker[] Returns an array of VehicleMaker objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('v')
-//            ->andWhere('v.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('v.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?VehicleMaker
-//    {
-//        return $this->createQueryBuilder('v')
-//            ->andWhere('v.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findMakersByVehicleType(string $type): array
+    {
+        return $this->createQueryBuilder('vm')
+            ->join('vm.vehicles', 'v')
+            ->where('v.type = :type')
+            ->setParameter('type', $type)
+            ->getQuery()
+            ->getResult();
+    }
 }
