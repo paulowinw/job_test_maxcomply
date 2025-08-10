a34822a095e6c9ea5403eef735d3574afeefb3ac
Paulo Lopes <paulo.lopes.hb@gmail.com>
2025-08-10 17:20:44 +0100

    Removing unneccesary migration and updating the managment of the fk for vehicle makers

--- migrations/Version20250805141520.php
+++ migrations/Version20250805141520.php
@@ -22,7 +22,7 @@ final class Version20250805141520 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE vehicle (id INT AUTO_INCREMENT NOT NULL, maker_id INT DEFAULT NULL, type VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, top_speed INT DEFAULT NULL, engine_type VARCHAR(255) DEFAULT NULL, engine_power INT DEFAULT NULL, fuel_type VARCHAR(255) DEFAULT NULL, length NUMERIC(10, 2) DEFAULT NULL, width NUMERIC(10, 2) DEFAULT NULL, height NUMERIC(10, 2) DEFAULT NULL, weight NUMERIC(10, 2) DEFAULT NULL, number_of_seats INT DEFAULT NULL, zero_to_hundred_time NUMERIC(10, 2) DEFAULT NULL, INDEX IDX_1B80E48668DA5EC3 (maker_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE vehicle_maker (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE vehicle ADD CONSTRAINT FK_1B80E48668DA5EC3 FOREIGN KEY (maker_id) REFERENCES vehicle_maker (id)');
+        $this->addSql('ALTER TABLE vehicle ADD CONSTRAINT FK_1B80E48668DA5EC3 FOREIGN KEY (maker_id) REFERENCES vehicle_maker (id) ON DELETE CASCADE;');
     }
 
     public function down(Schema $schema): void
