6ac5c415dd6efce35185e8b950acb3ea32da4c20
Paulo Lopes <paulo.lopes.hb@gmail.com>
2025-08-10 01:49:29 +0100

    Configuring the file security.yaml and adding the ApiTokenAuthenticator to check the credentials

---
+++ src/Security/ApiTokenAuthenticator.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class ApiTokenAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+{
+    public function supports(Request $request): ?bool
+    {
+        // Check for the "X-AUTH-TOKEN" header on every API request
+        return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $apiToken = $request->headers->get('X-AUTH-TOKEN');
+        if (null === $apiToken) {
+            throw new AuthenticationException('No API token provided.');
+        }
+
+        // In a real application, you would query a database to find a User
+        // associated with this token. For this test, we'll use a hardcoded value.
+        if ($apiToken !== 'my-secret-token') {
+            throw new AuthenticationException('Invalid API token.');
+        }
+
+        // Return a self-validating passport with a dummy user.
+        return new SelfValidatingPassport(
+            new UserBadge('admin') // We use the user "admin" defined in security.yaml
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // on success, let the request continue
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $data = [
+            'message' => 'Authentication failed: ' . $exception->getMessage()
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent.
+     * This redirects to the 'login'.
+     */
+    public function start(Request $request, ?AuthenticationException $authException = null): Response
+    {
+        $data = [
+            'message' => 'Authentication Required'
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+}
